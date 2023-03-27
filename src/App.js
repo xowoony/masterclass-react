22afc686510286bf997f4591f70dbf079930a15b
xowoony <trumanblack@hanmail.net>
2023-03-27 17:41:57 +0900

    styled-components 적용해보기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import styled from "styled-components";
 
-const Father = styled.div`  // 백틱
+const Father = styled.div`
+  // 백틱
   display: flex;
-`; 
+`;
+
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+
+const BoxTwo = styled.div`
+  background-color: yellow;
+  width: 100px;
+  height: 100px;
+`;
 
 function App() {
   return (
     <Father>
-      <div>dd</div>
+      <BoxOne></BoxOne>
+      <BoxTwo></BoxTwo>
     </Father>
   );
 }
