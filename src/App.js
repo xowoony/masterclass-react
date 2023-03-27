ca9d3d88cfb8ae9f0789e44b45970630d86ba66b
xowoony <trumanblack@hanmail.net>
2023-03-27 16:55:07 +0900

    styled-components 사용하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
+const Father = styled.div`  // 백틱
+  display: flex;
+`;
+
 function App() {
-  return null;
+  return (
+    <Father>
+      <div>dd</div>
+    </Father>
+  );
 }
 
 export default App;
