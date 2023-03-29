d2304cbb0da5a1613a2592c6ef00e07168c91340
xowoony <trumanblack@hanmail.net>
2023-03-29 17:59:11 +0900

    dark, light theme 만들기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import styled, { keyframes } from "styled-components";
 
+// Title 컴포넌트
+// index.js에서 만들어준 theme textColor에 접근해 볼 수 있다.
+const Title = styled.h1`
+  color: ${(props) =>
+    props.theme.textColor}; // props.theme.property이름 이라 작성해주면 된다.
+`;
+
 const Wrapper = styled.div`
   align-items: center;
   display: flex;
@@ -7,56 +14,13 @@ const Wrapper = styled.div`
   justify-content: center;
   width: 100vw;
   height: 100vh;
-`;
-
-// animation
-const rotationAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius: 0px;
-    background-color: red;
-  }
-  50% {
-    transform:rotate(360deg);
-    border-radius: 100px;
-    background-color: yellow;
-
-  }
-  100%{
-    transform:rotate(0deg);
-    border-radius: 0px;
-    background-color: blueviolet;
-  }
-  `;
-
-// Emoji
-const Emoji = styled.span`
-font-size: 36px; // 기본값 36px
-`;  
-
-// Box
-const Box = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
- 
-  height: 200px;
-  width: 200px;
-  animation: ${rotationAnimation} 10s linear infinite;
-  // target처리
-  ${Emoji}:hover{
-    font-size: 100px;
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="div">🐠</Emoji>
-      </Box>
-      <Emoji as="div">🐷</Emoji> {/*얘는 타겟팅에 해당되지 않음*/}
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
